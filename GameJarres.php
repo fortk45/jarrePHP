900f3f036e5b1996a1cac27c571edf87a59b7924
fk45 <135996558+fortk45@users.noreply.github.com>
2026-02-24 16:44:20 +0100

    level 2

--- GameJarres.php
+++ GameJarres.php
@@ -1,22 +1,31 @@
 <?php
-echo "\n\n5 jarres se trouvent devant vous... Dans l'une d'elle, la clé. \n"
-        ."Mais attention ! Une des jarres est piégée par un serpent !\n\n"
-        ."Alors ? Quelle jarre choisissez-vous ?\n";
+echo "\n\nA chaque salle, 5 jarres se trouveront devant vous... Dans l'une d'elle, la clé. \n"
+        ."Trouvez trois clés et vous êtes libre !\n"
+        ."Mais attention ! Une des jarres est piégée par un serpent qui vous prendra toutes vos clés !\n\n";
 
-$jarreP = rand(1,5); //random jarre snake
-$choice = 0;
+$keys = 0;
 
-while ($choice == 0){
-    echo "Numéro de votre choix : ";
-    fscanf(STDIN, "%d", $choice); //the user enter a choice
-    if ($choice > 5 || $choice < 1){
-        $choice = 0;
-        echo "Erreur : Entrer un nombre entre 1 et 5.\n\n";
+while ($keys < 3) {
+    echo "Vous arrivez à la salle suivante...\n";
+    $jarreP = rand(1,5); //random jarre snake
+    $choice = 0;
+
+    while ($choice == 0){
+        echo "Numéro de votre choix : ";
+        fscanf(STDIN, "%d", $choice); //the user enter a choice
+        if ($choice > 5 || $choice < 1){
+            $choice = 0;
+            echo "Erreur : Entrer un nombre entre 1 et 5.\n\n";
+        }
+    }
+
+    //outcome
+    if ($choice == $jarreP){
+        $keys = 0;
+        echo "\nPerdu ! C'était la jarre piégée ! Le serpent vous saute dessus et vous vole toutes vos clés !\n\n";
+    } else {
+        $keys++;
+        echo "\nGagné ! Vous avez maintenant ".$keys." clés !\n\n";
     }
 }
-
-if ($choice == $jarreP){
-    echo "\nPerdu ! C'était la jarre piégée ! Le serpent vous saute dessus !";
-} else {
-    echo "\nGagné ! Vous êtes libre !";
-}
+echo "Vous avez réunis les trois clés ! Vous sortez enfin de ce donjon !\n\n";
