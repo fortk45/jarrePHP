1585eff1099f9c8530ce0b9a65ace18fb5a94180
fk45 <135996558+fortk45@users.noreply.github.com>
2026-02-24 16:45:17 +0100

    level 3 add difficulty

--- GameJarres.php
+++ GameJarres.php
@@ -1,13 +1,36 @@
 <?php
+
+function JarresRand($hard){
+    $snakes = [];
+    while (sizeof($snakes) < $hard){
+        $nb = rand(1,5);
+        if(!in_array($nb, $snakes)){
+            $snakes[] = $nb;
+        }
+    }
+    return $snakes;
+}
+
+//choix de la difficulté
+$hard = 0;
+while ($hard == 0){
+    echo "Choisissez votre difficulté de 1 à 3 : ";
+    fscanf(STDIN, "%d", $hard); //the user enter a choice
+    if ($hard > 3 || $hard < 1){
+        $hard = 0;
+        echo "Erreur : Entrer un nombre entre 1 et 3.\n\n";
+    }
+}
+
 echo "\n\nA chaque salle, 5 jarres se trouveront devant vous... Dans l'une d'elle, la clé. \n"
         ."Trouvez trois clés et vous êtes libre !\n"
-        ."Mais attention ! Une des jarres est piégée par un serpent qui vous prendra toutes vos clés !\n\n";
+        ."Mais attention ! Certaines jarres sont piégées par un serpent qui vous prendra toutes vos clés !\n\n";
 
 $keys = 0;
 
 while ($keys < 3) {
     echo "Vous arrivez à la salle suivante...\n";
-    $jarreP = rand(1,5); //random jarre snake
+    $jarreP = JarresRand($hard); //random jarre snake
     $choice = 0;
 
     while ($choice == 0){
@@ -20,9 +43,9 @@ while ($keys < 3) {
     }
 
     //outcome
-    if ($choice == $jarreP){
+    if (in_array($choice, $jarreP)){
         $keys = 0;
-        echo "\nPerdu ! C'était la jarre piégée ! Le serpent vous saute dessus et vous vole toutes vos clés !\n\n";
+        echo "\nPerdu ! Cette jarre était piégée ! Le serpent vous saute dessus et vous vole toutes vos clés !\n\n";
     } else {
         $keys++;
         echo "\nGagné ! Vous avez maintenant ".$keys." clés !\n\n";
